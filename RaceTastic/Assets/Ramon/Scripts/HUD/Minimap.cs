bb6d83f951d84c56c4b2b16764e61070b613359e
KoningSherwood <ramonzwiep@hotmail.com>
2021-12-17 11:39:24 +0100

    @everyone Ok (edited)

--- RaceTastic/Assets/Ramon/Scripts/HUD/Minimap.cs
+++ RaceTastic/Assets/Ramon/Scripts/HUD/Minimap.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class Minimap : MonoBehaviour
 {
     public Transform player;
+    public Vector3 offset;
 
     private void LateUpdate()
     {
+        /*
         Vector3 newPosition = player.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
 
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        */
+
+        Vector3 newPosition = player.position;
+        newPosition.y += offset.y;
+        transform.position = newPosition;
     }
 }
---
+++ RaceTastic/Assets/Ramon/Scripts/Skin Change/ChangeSkin.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSkin : MonoBehaviour
+{
+    public List<GameObject> carSkins, bikeSkins;
+    public GameObject selectedVehicle;
+
+    private int skinIndex;
+    public int maxIndex, minIndex;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    public void switchSkins(int index)
+    {
+        Debug.Log("switchingSkins");
+
+        skinIndex += index;
+
+        if (skinIndex < maxIndex)
+        {
+            skinIndex = minIndex;
+        }
+
+        if (skinIndex > minIndex)
+        {
+            skinIndex = maxIndex;
+        }
+
+        selectedVehicle = carSkins[skinIndex];
+    }
+}
