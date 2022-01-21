46ef2f94136fae724a731643a56b2fbe03723571
KoningSherwood <ramonzwiep@hotmail.com>
2022-01-21 13:44:09 +0100

    Player Preference Settings

--- Racetastic_URP/Assets/Ramon/Scripts/Menu/SettingsMenu.cs
+++ Racetastic_URP/Assets/Ramon/Scripts/Menu/SettingsMenu.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 using TMPro;
 
 public class SettingsMenu : MonoBehaviour
 {
     public TextMeshProUGUI masterText, sfxText, musicText;
+    public Slider masterSlider, sfxSlider, musicSlider;
+    public TMP_Dropdown qualityDropdown;
     public AudioMixer mixer;
-    public float offset, defaultVolume;
+    public float offset;
     private string masterString, sfxString, musicString;
 
     private void Start()
@@ -17,13 +20,17 @@ public class SettingsMenu : MonoBehaviour
         sfxString = sfxText.text;
         musicString = musicText.text;
 
-        SetMasterVolume(defaultVolume);
-        SetSFXVolume(defaultVolume);
-        SetMusicVolume(defaultVolume);
+        masterSlider.value = PlayerPrefs.GetFloat("Master Slider");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX Slider");
+        musicSlider.value = PlayerPrefs.GetFloat("Music Slider");
+
+        qualityDropdown.value = PlayerPrefs.GetInt("Quality Index");
     }
 
     public void SetMasterVolume(float volume)
     {
+        PlayerPrefs.SetFloat("Master Slider", masterSlider.value);
+
         mixer.SetFloat("Master", volume);
         volume += offset;
         masterText.text = masterString + volume.ToString("f0") + "%";
@@ -31,16 +38,18 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetSFXVolume(float volume)
     {
-        mixer.SetFloat("SFX", volume);
+        PlayerPrefs.SetFloat("SFX Slider", sfxSlider.value);
 
+        mixer.SetFloat("SFX", volume);
         volume += offset;
         sfxText.text = sfxString + volume.ToString("f0") + "%";
     }
 
     public void SetMusicVolume(float volume)
     {
-        mixer.SetFloat("Music", volume);
+        PlayerPrefs.SetFloat("Music Slider", musicSlider.value);
 
+        mixer.SetFloat("Music", volume);
         volume += offset;
         musicText.text = musicString + volume.ToString("f0") + "%";
     }
@@ -52,6 +61,8 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetQuality(int qualityIndex)
     {
+        PlayerPrefs.SetInt("Quality Index", qualityIndex);
+
         QualitySettings.SetQualityLevel(qualityIndex);
         Debug.Log(QualitySettings.GetQualityLevel());
     }
